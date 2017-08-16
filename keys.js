bdf35e7c91564cc8eb80dcb7a385c0a1262a4574
mario@dhali.com <mario@dhali.com>
2017-08-16 09:21:09 -0700

    Added keys.js with twitter keys/tokens

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '	UL0suVSO8XwysXAQZvvWA4bOU',
+  consumer_secret: 'l528SDLotPFqe9vWEtH9PXWyGAJcgH4Kmjxk8ZSeDIf7918glI',
+  access_token_key: '34052073-SWsLvjaxXHPiV38ZXbGsTJpBOcbiOUAotkk9DSeMe',
+  access_token_secret: '3Gj4y27fAPvoplf43Cf67zD5pEuYgypcYaxsynk1s96mZ',
+}
