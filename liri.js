10a0ef19ed4570c03f69d78bd2efea2c56a9fd96
mario@dhali.com <mario@dhali.com>
2017-08-16 12:56:15 -0700

    Added package.json, Added node_modules

--- liri.js
+++ liri.js
@@ -1,1 +1,55 @@
+// Load Required files
 var twitter = require('./keys.js');
+var omdb = require('./node_modules/omdb');
+
+var task = process.argv[2];
+
+omdb.get({ title: 'Saw', year: 2004 }, true, function(err, movie) {
+    if(err) {
+        return console.error(err);
+    }
+
+    if(!movie) {
+        return console.log('Movie not found!');
+    }
+
+    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    console.log(movie.plot);
+
+    // Saw (2004) 7.6/10
+    // Two men wake up at opposite sides of a dirty, disused bathroom, chained
+    // by their ankles to pipes. Between them lies...
+});
+
+// Movie This
+// node liri.js movie-this '<movie name here>'
+if ( task === 'movie-this') {
+	// var omdbapikey = '40e9cece';
+	// var omdbapi = 'http://www.omdbapi.com/?apikey=' + omdbapikey;
+
+	// var movieTitle = process.argv[3];
+	// var requestUrl = omdbapi + '&t=' + movieTitle;
+
+	// console.log( omdbapi );
+	// console.log( requestUrl );
+
+
+
+	omdb.get({ title: 'Saw', year: 2004 }, true, function(err, movie) {
+	    if(err) {
+	        return console.error(err);
+	    }
+
+	    if(!movie) {
+	        return console.log('Movie not found!');
+	    }
+
+	    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+	    console.log(movie.plot);
+
+	    // Saw (2004) 7.6/10
+	    // Two men wake up at opposite sides of a dirty, disused bathroom, chained
+	    // by their ankles to pipes. Between them lies...
+	});
+
+}
