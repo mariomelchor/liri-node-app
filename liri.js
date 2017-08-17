77c2bb563b78545a49e55c8aaedd807d04643306
mariomelchor <mario@mariomelchor.com>
2017-08-16 22:07:33 -0700

    Added error check and checked if movie response is True

--- liri.js
+++ liri.js
@@ -14,37 +14,45 @@ if ( task === 'movie-this') {
   var requestUrl = omdbapi + '&type=movie&plot=short&t=' + movieTitle;
 
   request(requestUrl, function (error, response, body) {
+    var movie = JSON.parse(body);
 
-    if (error) {
+    // If there is an error show message
+    if ( error ) {
       return console.log( 'Sorry Please Try Searching Again there was an Error.' );
     }
 
-    var movie = JSON.parse(body);
+    // If there is a Movie show movie data
+    if ( movie.Response === 'True' ) {
 
-    console.log( '******************************************************************************************************************' );
-    console.log( '******************************************       MOVIE THIS       ************************************************' );
-    console.log( '' );
-
-    if ( movie.Title === 'Mr. Nobody' ) {
-      console.log( 'If you havent watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/ Its on Netflix!');
+      console.log( '******************************************************************************************************************' );
+      console.log( '******************************************       MOVIE THIS       ************************************************' );
       console.log( '' );
-    }
 
-    console.log( 'MOVIE INFORMATION' );
-    console.log( 'Movie Title: ' + movie.Title );
-    console.log( 'Year Released: ' + movie.Year );
-    console.log( 'Country: ' + movie.Country );
-    console.log( 'Language: ' + movie.Language );
-    console.log( '' );
-    console.log( 'RATINGS' );
-    console.log( movie.Ratings[0].Source + ': ' + movie.Ratings[0].Value );
-    console.log( movie.Ratings[1].Source + ': ' + movie.Ratings[1].Value );
-    console.log( '' );
-    console.log( 'PLOT/ACTORS' );
-    console.log( 'Plot: ' + movie.Plot );
-    console.log( 'Actors: ' + movie.Actors );
-    console.log( '' );
-    console.log( '******************************************************************************************************************' );
+      // If no movie is passed show this movie
+      if ( movie.Title === 'Mr. Nobody' ) {
+        console.log( 'If you havent watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/ Its on Netflix!');
+        console.log( '' );
+      }
+
+      console.log( 'MOVIE INFORMATION' );
+      console.log( 'Movie Title: ' + movie.Title );
+      console.log( 'Year Released: ' + movie.Year );
+      console.log( 'Country: ' + movie.Country );
+      console.log( 'Language: ' + movie.Language );
+      console.log( '' );
+      console.log( 'RATINGS' );
+      console.log( movie.Ratings[0].Source + ': ' + movie.Ratings[0].Value );
+      console.log( movie.Ratings[1].Source + ': ' + movie.Ratings[1].Value );
+      console.log( '' );
+      console.log( 'PLOT/ACTORS' );
+      console.log( 'Plot: ' + movie.Plot );
+      console.log( 'Actors: ' + movie.Actors );
+      console.log( '' );
+      console.log( '******************************************************************************************************************' );
+
+    } else {
+      console.log( 'Movie Not Found!' );
+    }
 
   });
 }
