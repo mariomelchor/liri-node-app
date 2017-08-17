ca02b86d414ba2bfaf2ed43007b6a29464aee210
mariomelchor <mario@mariomelchor.com>
2017-08-16 20:33:21 -0700

    Added node_modules, Added request module, omdb response workign

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 // Load Required files
 var twitter = require('./keys.js');
+var request = require('request');
 
 var task = process.argv[2];
 
@@ -12,6 +13,14 @@ if ( task === 'movie-this') {
 	var movieTitle = process.argv[3];
 	var requestUrl = omdbapi + '&t=' + movieTitle;
 
-	console.log( omdbapi );
-	console.log( requestUrl );
+	// console.log( omdbapi );
+	// console.log( requestUrl );
+
+	request(requestUrl, function (error, response, body) {
+	  // console.log('error:', error); // Print the error if one occurred
+	  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  // console.log('body:', body); // Print the HTML for the Google homepage.
+
+	  var movie = JSON.parse(body);
+	});
 }
