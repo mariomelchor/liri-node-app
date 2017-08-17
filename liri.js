6fc603aadae2fb4e109a9457ffe18c0749505b09
mariomelchor <mario@mariomelchor.com>
2017-08-16 21:52:46 -0700

    Formatted console logging

--- liri.js
+++ liri.js
@@ -10,36 +10,40 @@ if ( task === 'movie-this') {
   var omdbapikey = '40e9cece';
   var omdbapi = 'http://www.omdbapi.com/?apikey=' + omdbapikey;
 
-  var movieTitle = process.argv[3];
+  var movieTitle = process.argv[3] || 'Mr. Nobody';
   var requestUrl = omdbapi + '&type=movie&plot=short&t=' + movieTitle;
 
-  // console.log( omdbapi );
-  // console.log( requestUrl );
-
   request(requestUrl, function (error, response, body) {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+
+    if (error) {
+      return console.log( 'Sorry Please Try Searching Again there was an Error.' );
+    }
 
     var movie = JSON.parse(body);
 
     console.log( '******************************************************************************************************************' );
     console.log( '******************************************       MOVIE THIS       ************************************************' );
-    console.log( '                                                                                                                  ' );
-    console.log( 'MOVIE INFORMATION                                                                                                           ' );
+    console.log( '' );
+
+    if ( movie.Title === 'Mr. Nobody' ) {
+      console.log( 'If you havent watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/ Its on Netflix!');
+      console.log( '' );
+    }
+
+    console.log( 'MOVIE INFORMATION' );
     console.log( 'Movie Title: ' + movie.Title );
     console.log( 'Year Released: ' + movie.Year );
     console.log( 'Country: ' + movie.Country );
     console.log( 'Language: ' + movie.Language );
-    console.log( '                                                                                                                  ' );
-    console.log( 'RATINGS                                                                                                           ' );
+    console.log( '' );
+    console.log( 'RATINGS' );
     console.log( movie.Ratings[0].Source + ': ' + movie.Ratings[0].Value );
     console.log( movie.Ratings[1].Source + ': ' + movie.Ratings[1].Value );
-    console.log( '                                                                                                                  ' );
-    console.log( 'PLOT/ACTORS                                                                                                           ' );
+    console.log( '' );
+    console.log( 'PLOT/ACTORS' );
     console.log( 'Plot: ' + movie.Plot );
     console.log( 'Actors: ' + movie.Actors );
-    console.log( '                                                                                                                  ' );
+    console.log( '' );
     console.log( '******************************************************************************************************************' );
 
   });
